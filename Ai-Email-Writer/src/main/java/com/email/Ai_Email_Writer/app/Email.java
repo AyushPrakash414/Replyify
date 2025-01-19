ca2cb8c7bf89934f3bd47d59bedb9ec6b531a28b
Ayush <prakashayush414@gmail.com>
2025-01-19 18:13:18 +0530

    changes in Email class

--- Ai-Email-Writer/src/main/java/com/email/Ai_Email_Writer/app/Email.java
+++ Ai-Email-Writer/src/main/java/com/email/Ai_Email_Writer/app/Email.java
@@ -2,7 +2,7 @@ package com.email.Ai_Email_Writer.app;
 
 import lombok.Data;
 
-@Data      //--->Help to Create the Getter And Setter for Email Class (lombok)
+@Data      //--->Help to Create the Getter And Setter for Email Class (lombok).....
 public class Email {
      private String emailContent;
      private String tone;
